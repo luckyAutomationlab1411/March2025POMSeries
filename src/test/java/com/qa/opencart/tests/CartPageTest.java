f54c00e676aadb848ec172f6ef90df48873c86d9
LuckyStar <priyabratap.1411@gmail.com>
2025-09-08 20:10:36 +0530

    added cart test

---
+++ src/test/java/com/qa/opencart/tests/CartPageTest.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+public class CartPageTest {
+	
+	@Test
+	public void cardTest() {
+		System.out.println("cart testing");
+	}
+
+}
